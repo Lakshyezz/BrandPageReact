f55a423b1ef63fc1f7196cd05f03d44e82cdaf27
lakshay deep <lakshay@lakshays-MacBook-Air.local>
2023-09-14 03:00:41 +0530

    ok

---
+++ src/components/navigation.jsx
@@ -0,0 +1,18 @@
+const Navigation = () =>{
+    return (
+        <nav className="container">
+        <div className="logo">
+                <img src="/images/brand_logo.png" alt="logologo" /> 
+        </div>
+        <ul>
+            <li>Menu</li>
+            <li>Location</li>
+            <li>About</li>
+            <li>Button</li>
+        </ul>
+        <button> Login </button>
+    </nav>
+    );
+};
+
+export default Navigation;
